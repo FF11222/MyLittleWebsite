2b5673f59ee95589a3570539a48b4a82bf402e71
Patrick <tingyuchang960625@gmail.com>
2024-02-03 22:05:57 +0800

    initial commit

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+    <ul>
+      <li id="logo">
+        <img src="../assets/logo.png" alt="logo">
+      </li>
+      <li></li>
+      <li></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+
+export default ({
+  name: "NavigationBar"
+})
+</script>
+
+<style scoped>
+
+</style>
